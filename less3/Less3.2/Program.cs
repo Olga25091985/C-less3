a59c25b171dbcad5be105e907963c2b2d7d3b1b2
Olga <helga_happy@mail.ru>
2023-01-11 14:12:29 +0300

    Программа, которая принимает на вход два числа (А и В) и возводит число А в натуральную степень В

--- less3/Less3.2/Program.cs
+++ less3/Less3.2/Program.cs
@@ -7,12 +7,31 @@
 // 5 -> 1, 8, 27, 64, 125
 
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[num];
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[num];
 
-for (int index = 0; index < num; index++) 
+// for (int index = 0; index < num; index++) 
+// {
+//     array[index] = ((index + 1) * (index + 1) * (index + 1));
+//     Console.WriteLine(array[index]);
+// }
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе  число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[2] { num1, num2 };
+for (int index = 0; index < num2; index++)
 {
-    array[index] = ((index + 1) * (index + 1) * (index + 1));
-    Console.WriteLine(array[index]);
+int i = Convert.ToInt32(Math.Pow(num1, num2));
+ Console.WriteLine("A ^ B = " + Math.Pow(num1, num2));
 }
+// int i = Convert.ToInt32(Math.Pow(5, 3))
